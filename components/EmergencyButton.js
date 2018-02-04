a0666d60c698fdc59fafef4a494cacd51b2331dc
ylafontaine <yannicklafontaine@deepteal.com>
2018-02-04 02:02:12 -0500

    add geoposition to alrt call and catch

--- components/EmergencyButton.js
+++ components/EmergencyButton.js
@@ -11,6 +11,16 @@ export class EmergencyButton extends React.Component {
     this._handleEmergencyPress = this._handleEmergencyPress.bind(this)
   }
 
+  state = {
+    token: null
+  };
+
+  componentWillMount() {
+    Notifications.getExpoPushTokenAsync().then((resp) => {
+      this.setState({ token: resp });
+    });
+  }
+
   render() {
     return (
       <TouchableOpacity onPress={this._handleEmergencyPress}>
@@ -20,21 +30,27 @@ export class EmergencyButton extends React.Component {
   }
 
   async _handleEmergencyPress() {
-    let token = await Notifications.getExpoPushTokenAsync();
+    console.log("coco");
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var x = position.coords.longitude;
+        var y = position.coords.lattitude;
 
-    fetch(ServerAPI.alerts, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+         fetch(ServerAPI.alerts, {
+           headers: {
+             'Accept': 'application/json',
+             'Content-Type': 'application/json'
+           },
+           method: "POST",
+           body: JSON.stringify({ position: { x: x, y: y }, token: this.state.token })
+         });
+
+         alarm.play()
+         this.props.onClick()
       },
-      body: JSON.stringify({
-        token: token
-      }),
-    });
-
-    alarm.play()
-    this.props.onClick()
+      (error) => { console.log("error", error); this.setState({ error: error.message }) },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
   }
 }
 
