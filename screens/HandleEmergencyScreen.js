40d7e173e45239540f1c623b4c13b24e41eb39e2
Vincent Cossette <vcossette@simexperts.com>
2018-02-04 11:12:13 -0500

    Buttons

--- screens/HandleEmergencyScreen.js
+++ screens/HandleEmergencyScreen.js
@@ -1,11 +1,56 @@
 import React from 'react'
 
-import { Text } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+
 
 export default class HandleEmergencyScreen extends React.Component {
   render() {
     return (
-      <Text> Allo </Text>
+      <View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons
+              name={'ios-navigate'}
+              size={28}
+              style={styles.buttonIcon}
+            />
+            <Text style={styles.buttonText}>Navigate</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons
+              name={'ios-navigate'}
+              size={28}
+              style={styles.buttonIcon}
+            />
+            <Text style={styles.buttonText}>Navigate</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#fefefe',
+    flex: 1,
+    paddingTop: 3,
+  },
+  buttonContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  buttonIcon: {
+    color: '#999'
+  },
+  buttonText: {
+    color: '#999',
+    fontSize: 10
+  },
+})
