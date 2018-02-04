155d89cb594962af8dceadef261a30201e716f09
J-F Gelinas <jfgelinas@J-Fs-MacBook-Pro.local>
2018-02-04 11:57:43 -0500

    More styling for the emergency handling page

--- screens/HandleEmergencyScreen.js
+++ screens/HandleEmergencyScreen.js
@@ -9,27 +9,42 @@ export default class HandleEmergencyScreen extends React.Component {
     return (
       <View style={styles.pageContainer}>
         <View style={styles.alertContainer}>
-          <Text>Insert some shit here...</Text>
+          <View style={styles.alertTitle}>
+            <Ionicons
+              name={'ios-warning'}
+              size={56}
+              style={styles.warningButtonIcon}
+            />
+            <Text style={styles.warningText}>Insert some title here...</Text>
+            <Ionicons
+              name={'ios-warning'}
+              size={56}
+              style={styles.warningButtonIcon}
+            />
+          </View>
+          <View style={styles.alertBody}>
+            <Text style={styles.warningText}>The alert body goes here...</Text>
+          </View>
         </View>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <TouchableOpacity style={styles.button}>
               <Ionicons
-                name={'ios-navigate'}
+                name={'ios-thumbs-up'}
                 size={28}
                 style={styles.buttonIcon}
               />
-              <Text style={styles.buttonText}>Navigate</Text>
+              <Text style={styles.buttonText}>Yes</Text>
             </TouchableOpacity>
           </View>
           <View style={styles.buttonContainer}>
             <TouchableOpacity style={styles.button}>
               <Ionicons
-                name={'ios-navigate'}
+                name={'ios-thumbs-down'}
                 size={28}
                 style={styles.buttonIcon}
               />
-              <Text style={styles.buttonText}>Navigate</Text>
+              <Text style={styles.buttonText}>No</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -41,19 +56,29 @@ export default class HandleEmergencyScreen extends React.Component {
 const styles = StyleSheet.create({
   alertContainer: {
     alignItems: 'center',
+    flex: 1
+  },
+  alertTitle: {
+    alignItems: 'center',
     flex: 1,
-    paddingTop: 50
+    flexDirection: 'row'
+  },
+  alertBody: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row'
   },
   button: {
     alignItems: 'center',
     backgroundColor: '#fefefe',
+    borderWidth: 1,
+    borderColor: '#999',
     flex: 1,
+    margin: 20,
     paddingTop: 3,
   },
   buttonContainer: {
     alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
     flex: 1,
     flexDirection: 'row'
   },
@@ -69,6 +94,22 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   pageContainer: {
-    flex: 1
+    borderColor: '#ce2029',
+    borderWidth: 4,
+    borderRadius: 12,
+    flex: 1,
+    margin: 20,
+    marginBottom: 100
+  },
+  warningButtonIcon: {
+    color: '#ce2029',
+    flex: 1,
+    margin: 10
+  },
+  warningText: {
+    alignItems: 'center',
+    color: '#ce2029',
+    flex: 4,
+    textAlign: 'center'
   }
 })
